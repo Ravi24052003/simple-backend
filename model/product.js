9bd508a47fcc25de1f212c2e187b3ee5c8805313
Ravi24052003 <ravibusinessac291@gmail.com>
2024-02-02 20:04:26 +0530

    modified product.js

--- model/product.js
+++ model/product.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose');
 
 // Schema
 const productSchema = new mongoose.Schema({
-    title : {type: String, required: true},
+    title : {type: String, required: true, unique: [true, "Title should be unique"]},
     description : String,
-    price : {type: Number, min: [0, 'wrong price'], required: true},
+    price : {type: Number, min: [0, 'wrong price'], required: [true, "price is required"]},
     discountPercentage : {type: Number, min: [0, 'wrong min discount'], max: [50, 'wrong max discount']},
     rating : {type: Number, min: [0, 'wrong min rating'], max: [5, 'wrong max rating'], default: 0},
     stock : Number,
-    brand : {type: String, required: true},
-    category : {type: String, required: true},
-    thumbnail : {type: String, required: true},
+    brand : {type: String, required: [true, "Brand is required"]},
+    category : {type: String, required: [true, "Category is required"]},
+    thumbnail : {type: String, required: [true, "Thumbnail is required"]},
     images: [String]
   });
 
